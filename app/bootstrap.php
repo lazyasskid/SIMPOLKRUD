101a1a546677ee79c8e241bd104ee9f8d5556849
lazyasskid <lzysskd@gmail.com>
2020-11-05 23:42:33 +0800

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
     // Load libraries
     require_once 'libraries/Core.php';
-    require_once 'libraries/Controllers.php';
+    require_once 'libraries/Controller.php';
     require_once 'libraries/Database.php';
