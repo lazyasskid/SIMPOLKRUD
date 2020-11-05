7d34d8bdf1a52bea970a4e64058dddde3260bc3c
lazyasskid <lzysskd@gmail.com>
2020-11-06 00:06:51 +0800

    Update Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -12,9 +12,31 @@
 
         public function __construct() {
             $this->get_url();
+
+            // Look in controllers for first value
+            if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                // If exists, set as controller
+                $this->currentController = ucwords($url[0]);
+                // Unset 0 index
+                unset($url[0]);
+            }
+            
+            // Require the controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+            // Instantiate controller
+            $this->currentController = new $this->currentController;
         }
 
         public function get_url() {
-            
+            if(isset($_GET['url'])) {
+                // Remove forward slash
+                $url = rtrim($_GET['url'], '/'); 
+                // Make sure URL has no illegal characters
+                $url = filter_var($url, FILTER_SANITIZE_URL); 
+                // Convert URL into array
+                $url = explode('/', $url);
+
+                return $url;
+            }
         }
     }
