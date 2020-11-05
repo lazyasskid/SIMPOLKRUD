a4b1ec2162f28028fc1a7171870b9e520b9b01a2
lazyasskid <lzysskd@gmail.com>
2020-11-06 00:31:24 +0800

    Update Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -25,6 +25,23 @@
             require_once '../app/controllers/' . $this->currentController . '.php';
             // Instantiate controller
             $this->currentController = new $this->currentController;
+            
+            // Check for $url[1] 
+            if(isset($url[1])) {
+                // Check if method exists
+                if(method_exists($this->currentController, $url[1])) {
+                    // Set as current method
+                    $this->currentMethod = $url[1];
+                    // Unset 1 index
+                    unset($url[1]);
+                }
+            }
+            
+            // Get params 
+            $this->params =  $url ? array_values($url) : [];
+
+            // Call a callback with array of params
+            call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
         }
 
         public function get_url() {
