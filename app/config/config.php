675d15e7b82e7b6b9fbdfcb8e02b9c03d904e005
lazyasskid <lzysskd@gmail.com>
2020-11-08 23:59:58 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'eskul_user');
+    define('DB_PASS', 'eskul_pass');
+    define('DB_NAME', 'eskul_db');
+
     // App Root
     define('APP_ROOT', dirname(dirname(__FILE__)));
     // URL Root
