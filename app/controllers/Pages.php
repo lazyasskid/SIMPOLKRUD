c3ffb35694d0c0b198b996f0d66f1e8ee2ddb7a8
lazyasskid <lzysskd@gmail.com>
2020-11-08 23:30:00 +0800

    Update Pages.php

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,15 +1,19 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct() {
-            // ...
+
         }
 
         public function index() {
-            // ...
+            $data = [
+                'title' => 'phpApp-CRUD'
+            ];
+
+            $this->view('pages/index', $data);
         }
 
         public function about() {
-            // ...
+            $this->view('pages/about');
         }
     }
     
