a83ea9653b0718deb8a2df9389c3a734676bfa5c
lazyasskid <lzysskd@gmail.com>
2020-11-09 01:51:44 +0800

    Update about.php

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,5 +1,12 @@
 <?php require_once APP_ROOT . '/views/inc/header.php'; ?>
     <h1 class="display-3"><?php echo $data['title']; ?></h1>
     <p class="lead"><?php echo $data['description']; ?></p>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus faucibus ante quis ipsum facilisis tempor. Curabitur ultrices nisl rhoncus,
+    porttitor tellus id, euismod arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin lacinia sed
+    velit sed pellentesque. Aliquam erat volutpat. Ut sollicitudin massa at felis vulputate, vel bibendum ligula laoreet. Vivamus facilisis convallis
+    turpis, a sagittis enim pharetra a. Nam consectetur velit sed elit pellentesque, et imperdiet lorem posuere.
+    Pellentesque ut mi quis justo tincidunt fermentum.</p>
+    <p class="lead"><strong>Version: </strong><?php echo APP_VERSION; ?><br>
+        <strong>GitHub: </strong><a href="https://git.io/JkfSP" target="_blank">https://git.io/JkfSP</a></p>
 <?php require_once APP_ROOT . '/views/inc/footer.php'; ?>
 
