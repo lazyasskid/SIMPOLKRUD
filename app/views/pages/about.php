d08a2f0e233d8279c20bc4e7587cbb6de901848c
lazyasskid <lzysskd@gmail.com>
2020-11-09 01:34:15 +0800

    Update about.php

---
+++ app/views/pages/about.php
@@ -0,0 +1,5 @@
+<?php require_once APP_ROOT . '/views/inc/header.php'; ?>
+    <h1 class="display-3"><?php echo $data['title']; ?></h1>
+    <p class="lead"><?php echo $data['description']; ?></p>
+<?php require_once APP_ROOT . '/views/inc/footer.php'; ?>
+
