8a52a52af976507eaa7b7faa1e7024ff47e3d9a7
lazyasskid <lzysskd@gmail.com>
2020-11-09 01:34:08 +0800

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -7,9 +7,13 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <!-- Custom CSS -->
         <link rel="stylesheet" type="text/css" href="<?php echo URL_ROOT; ?>/css/styles.css">
 
         <title><?php echo SITE_NAME; ?></title>
     </head>
     <body>
+    <?php require_once APP_ROOT . '/views/inc/navbar.php'; ?>
+    <div class="container">
