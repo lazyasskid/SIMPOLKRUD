1e204458426d2c69c1112392c48d0b1d322ad265
lazyasskid <lzysskd@gmail.com>
2020-11-08 23:29:50 +0800

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    // App Root
+    define('APP_ROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URL_ROOT', 'http://localhost/phpApp-crud');
+    // Site Name
+    define('SITE_NAME', 'phpApp-CRUD');
+
+
