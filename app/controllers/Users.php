56af1466a5bd7157eb24433afb0501fbbe7da83e
lazyasskid <lzysskd@gmail.com>
2020-11-10 03:31:08 +0800

    Update Users.php

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,7 +1,7 @@
 <?php
     class Users extends Controller {
         public function __construct() {
-
+            $this->userModel = $this->model('User');
         }
 
         public function register() {
@@ -24,6 +24,11 @@
                 // Validate Email
                 if(empty($data['email'])) {
                     $data['email_err'] = 'Please enter email';
+                } else {
+                    // Check email
+                    if($this->userModel->findUserByEmail($data['email'])) {
+                        $data['email_err'] = 'Email is already taken';
+                    }
                 }
                 // Validate Name
                 if(empty($data['name'])) {
