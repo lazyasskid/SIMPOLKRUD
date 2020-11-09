498211eaac9c7e0151466cbd4270037b2727e29f
lazyasskid <lzysskd@gmail.com>
2020-11-10 02:20:32 +0800

    Create register.php

---
+++ app/views/users/register.php
@@ -0,0 +1,33 @@
+<?php require_once APP_ROOT . '/views/inc/header.php'; ?>
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card card-body bg-light mt-5">
+                <h2>Create An Account</h2>
+                <p>Please fill out this form to register</p>
+                <form action="<?php echo URL_ROOT; ?>/users/" method="post">
+                    <div class="form-group">
+                        <label for="name">Name: <sup>*</sup></label>
+                        <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? is-invalid : ''; ?> value="<?php echo $data['name']; ?>">
+                        <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Email: <sup>*</sup></label>
+                        <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? is-invalid : ''; ?> value="<?php echo $data['email']; ?>">
+                        <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Password: <sup>*</sup></label>
+                        <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? is-invalid : ''; ?> value="<?php echo $data['password']; ?>">
+                        <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Confirm Password: <sup>*</sup></label>
+                        <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? is-invalid : ''; ?> value="<?php echo $data['confirm_password']; ?>">
+                        <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php require_once APP_ROOT . '/views/inc/footer.php'; ?>
+
