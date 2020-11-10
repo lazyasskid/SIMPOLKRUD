1526beee4d8df12b735d73f36a0d407dd1c17ba8
lazyasskid <lzysskd@gmail.com>
2020-11-10 23:29:34 +0800

    Update navbar.php

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -18,6 +18,9 @@
             <ul class="navbar-nav ml-auto">
                 <?php if(isset($_SESSION['user_id'])) : ?>
                     <li class="nav-item">
+                        <a class="nav-link active" href="#">Welcome <?php echo $_SESSION['user_name']; ?></a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="<?php echo URL_ROOT; ?>/users/logout">Logout</a>
                     </li>
                 <?php else : ?>
