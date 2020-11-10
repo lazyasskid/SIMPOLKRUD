c70bd56f7adf3beb3cf2409b6ff3593274d07a8f
lazyasskid <lzysskd@gmail.com>
2020-11-10 21:58:55 +0800

    Create edit.php

---
+++ app/views/posts/edit.php
@@ -0,0 +1,21 @@
+<?php require_once APP_ROOT . '/views/inc/header.php'; ?>
+<a href="<?php echo URL_ROOT; ?>/posts/" class="btn btn-light"><i class="fa fa-backward"></i> Back</a>
+<div class="card card-body bg-light mt-5">
+    <h2>Edit Post</h2>
+    <p class="lead">Edit post below</p>
+    <form action="<?php echo URL_ROOT; ?>/posts/edit" method="post">
+        <div class="form-group">
+            <label for="name">Title: <sup>*</sup></label>
+            <input type="text" name='title' class="form-control form-control-lg <?php echo (!empty($data['title_err'])) ? 'is-invalid' : ''; ?> value="<?php echo $data['title']; ?>">
+            <span class="invalid-feedback"><?php echo $data['title_err']; ?></span>
+        </div>
+        <div class="form-group">
+            <label for="name">Body: <sup>*</sup></label>
+            <textarea type="text" name="body" class="form-control form-control-lg <?php echo (!empty($data['body_err'])) ? 'is-invalid' : ''; ?> "><?php echo $data['body'] ?></textarea>
+            <span class="invalid-feedback"><?php echo $data['body_err']; ?></span>
+        </div>
+        <input type="submit" class="btn btn-success" value="Submit">
+    </form>
+</div>
+<?php require_once APP_ROOT . '/views/inc/footer.php'; ?>
+
