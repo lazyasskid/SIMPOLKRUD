7be6df116cf813063ee430d0f76c2cc57aa0fd55
lazyasskid <lzysskd@gmail.com>
2020-11-10 19:50:33 +0800

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -5,4 +5,10 @@
         public function __construct() {
             $this->db = new Database;
         }
+
+        // Get Posts
+        public function getPosts() {
+            $this->db->query('SELECT *, posts.id as postId, users.id as userId, posts.created_at as postCreated, users.created_at as userCreated FROM posts INNER JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC');
+            return $this->db->resultSet();
+        }
     }
