ba4c9b3ac0ff9b80a27d5e9b744db7b0348f2b74
lazyasskid <lzysskd@gmail.com>
2020-11-10 19:50:29 +0800

    Update Posts.php

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,16 +1,21 @@
 <?php
     class Posts extends Controller {
+        private $postModel;
 
         public function __construct() {
             // If not login, redirect to login page
             if(!isLoggedIn()) {
                 redirect('users/login');
             }
+            $this->postModel = $this->model('Post');
         }
 
         public function index() {
-            $data = [];
-
+            // Get posts
+            $posts = $this->postModel->getPosts();
+            $data = [
+                'posts' => $posts
+            ];
             $this->view('posts/index', $data);
         }
     }
