cab35dd6be760355f6e0a5fbf4b66c15562354b9
lazyasskid <lzysskd@gmail.com>
2020-11-10 20:35:27 +0800

    Update Posts.php

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -18,4 +18,48 @@
             ];
             $this->view('posts/index', $data);
         }
+
+        public function add() {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Sanitize POST array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => '',
+                ];
+                // Validate Title
+                if(empty($data['title'])) {
+                    $data['title_err'] = 'Please enter title';
+                }
+
+                // Validate Body
+                if(empty($data['body'])) {
+                    $data['body_err'] = 'Please enter text';
+                }
+
+                // Make sure no errors
+                if(empty($data['title_err']) && empty($data['body_err'])) {
+                    // Validated
+                    if($this->postModel->addPost()) {
+                        flash('post_added', 'Post Added!');
+                        redirect('posts/index');
+                    } else {
+                        die('Something went wrong.');
+                    }
+                } else {
+                    // Load view with errors
+                    $this->view('posts/add', $data);
+                }
+            } else {
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+                $this->view('posts/add', $data);
+            }
+        }
     }
